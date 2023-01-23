6e00a542f54e7061ee8c6aeb83d66767ba91230d
srg <skabassjob@gmail.com>
2023-01-24 03:59:23 +0400

    refactor: remove comments

--- src/ws-server/index.ts
+++ src/ws-server/index.ts
@@ -1,21 +1,6 @@
 import { WebSocketServer, createWebSocketStream } from 'ws';
 import { duplexDataHandler } from '../duplex-data-handler';
 
-// <- mouse_up {y px}
-// <- mouse_down {y px}
-// <- mouse_left {x px}
-// <- mouse_right {x px}
-
-// <- mouse_position
-// ---> mouse_position {x px},{y px}
-
-// <- draw_circle {px}
-// <- draw_rectangle {px} {px}
-// <- draw_square {px}
-
-// <- prnt_scrn
-// ---> prnt_scrn {base64 string (png buf)}
-
 export const createWSServer = (port: number) => {
   const wss = new WebSocketServer({ port }, () => {
     console.log(`Start web socket server on the ${port} port!`)
