2ec223c1e90a881370e9f5ee0d746d497b0fb794
srg <skabassjob@gmail.com>
2023-01-24 03:58:45 +0400

    feat: add try-catch

--- src/duplex-data-handler.ts
+++ src/duplex-data-handler.ts
@@ -4,24 +4,28 @@ import { mouseCommand } from "./commands/mouse";
 import { prntCommand } from "./commands/prnt";
 
 export const duplexDataHandler = (duplex: internal.Duplex, data: string) => {
-  const [fullCommand, ...value] = data.split(' ')
-  const [command, action] = fullCommand.split('_')
-
-  switch (command) {
-    case 'mouse':
-      mouseCommand(duplex, action, value)
-      break;
-
-    case 'draw':
-      drawCommand(duplex, action, value)
-      break;
-
-    case 'prnt':
-      prntCommand(duplex, action)
-      break;
+  try {
+    const [fullCommand, ...value] = data.split(' ')
+    const [command, action] = fullCommand.split('_')
   
-    default:
-      console.log(`What the hell? What's the command: ${command}?`);
-      break;
+    switch (command) {
+      case 'mouse':
+        mouseCommand(duplex, action, value)
+        break;
+  
+      case 'draw':
+        drawCommand(duplex, action, value)
+        break;
+  
+      case 'prnt':
+        prntCommand(duplex, action)
+        break;
+    
+      default:
+        console.log(`What the hell? What's the command: ${command}?`);
+        break;
+    }
+  } catch (err) {
+    console.log(err);
   }
 }
