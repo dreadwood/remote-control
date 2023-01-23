78e381fc49e6d58b2f21cb4421147a1148f8e77f
srg <skabassjob@gmail.com>
2023-01-24 02:02:29 +0400

    feat: mouse handler

--- index.ts
+++ index.ts
@@ -1,12 +1,9 @@
 import { HTTP_PORT, WS_PORT } from './src/const.js'
 import { httpServer } from './src/http-server/index.js'
 import { createWSServer } from './src/ws-server/index.js';
-// import { mouse } from "@nut-tree/nut-js";
 
 const httpPort = process.env.HTTP_PORT ? Number(process.env.HTTP_PORT) : HTTP_PORT;
 const wsPort = process.env.WS_PORT ? Number(process.env.WS_PORT) : WS_PORT;
-
-// console.log(wsPort, process.env.WS_PORT);
 
 console.log(`Start static http server on the ${httpPort} port!`)
 httpServer.listen(httpPort)
--- src/ws-server/index.ts
+++ src/ws-server/index.ts
@@ -1,14 +1,56 @@
-import { WebSocketServer } from 'ws';
+import { WebSocketServer, createWebSocketStream } from 'ws';
+import { mouseCommand } from '../commands/mouse';
+
+// <- mouse_up {y px}
+// <- mouse_down {y px}
+// <- mouse_left {x px}
+// <- mouse_right {x px}
+
+// <- mouse_position
+// ---> mouse_position {x px},{y px}
+
+// <- draw_circle {px}
+// <- draw_rectangle {px} {px}
+// <- draw_square {px}
+
+// <- prnt_scrn
+// ---> prnt_scrn {base64 string (png buf)}
 
 export const createWSServer = (port: number) => {
-  const wss = new WebSocketServer({ port });
+  const wss = new WebSocketServer({ port }, () => {
+    console.log(`Start web socket server on the ${port} port!`)
+  });
 
-  wss.on('connection', function connection(ws) {
-    ws.on('message', function message(data) {
-      console.log('received: %s', data);
-    });
+  wss.on('connection', async function connection(ws) {
+    console.log('connection start!');
+
+    const duplex = createWebSocketStream(ws, { encoding: 'utf8', decodeStrings: false });
+
+    duplex.on('data', (data) => {
+      const [fullCommand, ...value] = data.split(' ')
+      const [command, action] = fullCommand.split('_')
+
+      switch (command) {
+        case 'mouse':
+          mouseCommand(duplex, action, value)
+          break;
+
+        case 'draw':
+          
+          break;
+
+        case 'prnt':
+          
+          break;
+      
+        default:
+          console.log(`What the hell? What's the command: ${command}?`);
+          break;
+      }
+    })
+
   
-    ws.send('something');
+    ws.send('connected');
   });
 
   return wss;
